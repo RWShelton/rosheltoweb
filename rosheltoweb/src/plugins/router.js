c2528eac658a0cd62d256a8b1602ee28437be0d2
RWShelton <Robert-Shelton@live.com>
2024-05-03 01:54:09 -0700

    Updating paths to account for hash change

--- rosheltoweb/src/plugins/router.js
+++ rosheltoweb/src/plugins/router.js
@@ -1,8 +1,8 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-    {path: '/rosheltoweb/', name: 'home', component: () => import('@/pages/AboutMe.vue')},
-    {path: '/rosheltoweb/resume', name: 'resume', component: () => import('@/pages/Resume.vue')},
+    {path: '/', name: 'home', component: () => import('@/pages/AboutMe.vue')},
+    {path: '/resume', name: 'resume', component: () => import('@/pages/Resume.vue')},
     {path: '/:pathMatch(.*)', name: '404', component: () => import('@/pages/404.vue')}
 ]
 
